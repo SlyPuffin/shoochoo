c49a318e5472cbc3a07338535d7717dedfffc51c
SlyPuffin <dan.c.voss@gmail.com>
2023-07-19 18:11:36 -0700

    Add functional delete view accessible from detail view

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import TaskItem
 
 # Create your views here.
@@ -20,3 +21,8 @@ class TaskUpdateView(UpdateView):
     model = TaskItem
     template_name = "task_edit.html"
     fields = ["title", "body", "status", "elapsed_time", "estimated_time", "due_date"]
+
+class TaskDeleteView(DeleteView):
+    model = TaskItem
+    template_name = "task_delete.html"
+    success_url = reverse_lazy("home")
